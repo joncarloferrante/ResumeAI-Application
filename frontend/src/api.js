60a5367e28d9260cdbe03d88c727cde11c8b31cc
Joncarlo <Jferrante@Atlantic.com>
2026-07-21 14:54:59 -0400

    Fix production backend API URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const apiHost = window.location.hostname || "127.0.0.1";
+const apiBaseUrl = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
 
 export default axios.create({
-  baseURL: `http://${apiHost}:8000`,
+  baseURL: apiBaseUrl,
   withCredentials: true,
 });
